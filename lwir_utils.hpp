37ee5fb193e493e22cfe8388b2c91fc0478bf76c
Can Lehmann <can.l@posteo.de>
2026-09-10 02:04:03 +0200

    move ArenaAllocator from metajit.cpp to lwir.cpp

--- lwir_utils.hpp
+++ lwir_utils.hpp
@@ -14,7 +14,96 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#include <cassert>
+#include <cstdint>
+#include <cstdlib>
+#include <cstring>
+
 namespace lwir {
+  class ArenaAllocator {
+  private:
+    struct Chunk {
+      Chunk* next = nullptr;
+      uint8_t data[0];
+    };
+
+    static constexpr size_t CHUNK_SIZE = 1024 * 1024; // 1 MiB
+    static constexpr size_t USABLE_SIZE = CHUNK_SIZE - sizeof(Chunk);
+
+    Chunk* _first = nullptr;
+    Chunk* _current = nullptr;
+
+    size_t _left = 0;
+    uint8_t* _ptr = nullptr;
+
+    inline size_t align_pad(void* ptr, size_t align) {
+      size_t delta = (uintptr_t) ptr % align;
+      return delta ? align - delta : 0;
+    }
+  public:
+    ArenaAllocator() {
+      _first = (Chunk*) malloc(CHUNK_SIZE);
+      new (_first) Chunk();
+      _current = _first;
+    }
+
+    ~ArenaAllocator() {
+      Chunk* chunk = _first;
+      while (chunk) {
+        Chunk* next = chunk->next;
+        free(chunk);
+        chunk = next;
+      }
+    }
+
+    void* alloc(size_t size, size_t align) {
+      assert(size <= USABLE_SIZE);
+
+      size_t align_padding = align_pad(_ptr, align);
+
+      if (__builtin_expect(_left - align_padding < size, 0)) {
+        if (_current->next) {
+          _current = _current->next;
+        } else {
+          Chunk* chunk = (Chunk*) malloc(CHUNK_SIZE);
+          new (chunk) Chunk();
+          _current->next = chunk;
+          _current = chunk;
+        }
+        _left = USABLE_SIZE;
+        _ptr = _current->data;
+        align_padding = align_pad(_ptr, align);
+      }
+
+      _ptr += align_padding;
+      _left -= align_padding;
+      void* ptr = (void*) _ptr;
+      _ptr += size;
+      _left -= size;
+      return ptr;
+    }
+
+    template <class T>
+    T* alloc() {
+      return (T*) alloc(sizeof(T), alignof(T));
+    }
+
+    void dealloc_all() {
+      _current = _first;
+      _ptr = _first->data;
+      _left = USABLE_SIZE;
+    }
+
+    void zero_all() {
+      Chunk* chunk = _first;
+      while (chunk) {
+        std::memset(chunk->data, 0, USABLE_SIZE);
+        chunk = chunk->next;
+      }
+      dealloc_all();
+    }
+  };
+
   template <class T>
   class Span {
   private:
@@ -37,7 +126,8 @@ namespace lwir {
     T* data() const { return _data; }
 
     size_t size() const { return _size; }
-    
+    bool empty() const { return _size == 0; }
+
     inline T at(size_t index) const {
       assert(index < _size);
       return _data[index];
@@ -53,6 +143,9 @@ namespace lwir {
 
     T* begin() const { return _data; }
     T* end() const { return _data + _size; }
+
+    inline T front() const { return at(0); }
+    inline T back() const { return at(_size - 1); }
 
     Span zeroed() const {
       memset(_data, 0, sizeof(T) * _size);
